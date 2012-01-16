15473f410b3fad86498ac7bec4bb2786e0c7e68f
Ian Enders <ian.enders@gmail.com>
2012-01-15 18:58:47 -0800

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pfft::Application.config.secret_token = 'e13d27ba5f31c8321aef919dd8dd1a0ab5818b9289f2287721208051decb91f2232075317fbcc3ab739a09ac09d14aa5a14119f870dd41088ca08ad5679d0316'
